3db209a2b66e2c475ca5bd08eeff1872f93606ea
blankMCU <vexown@gmail.com>
2025-02-16 22:56:17 +0100

    Rework the script to use libcamera instead of the obsolete picamera

--- backup_camera_RPi0/start_camera_feed.py
+++ backup_camera_RPi0/start_camera_feed.py
@@ -1,37 +1,33 @@
-import picamera
 import sys
 import tty
 import termios
 import threading
+import subprocess
 
 def start_camera_preview():
-	with picamera.PiCamera() as camera:
-		camera.rotation = 270
-		camera.hflip = True
-		camera.start_preview()
-		camera.preview.fullscreen = True
-		camera.preview.alpha = 255
-		
-		print("Press any key to close camera preview.")
-		sys.stdin.read(1)
+    try:
+        print("Starting camera preview...")
+        subprocess.run(["libcamera-hello", "--hflip", "--vflip"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error starting camera: {e}")
 
 def keypress():
-	fd = sys.stdin.fileno()
-	old_settings = termios.tcgetattr(fd)
-	try:
-		tty.setraw(fd)
-		key = sys.stdin.read(1)
-	finally:
-		termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-	return key
-	
+    """ Wait for a keypress to exit the program. """
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(fd)
+        key = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return key
+
 if __name__ == "__main__":
-	camera_thread = threading.Thread(target=start_camera_preview)
-	camera_thread.start()
-	
-	print("Press any key to exit.")
-	keypress()
-	
-	camera_thread.join()
+    camera_thread = threading.Thread(target=start_camera_preview)
+    camera_thread.start()
 
+    print("Press any key to exit.")
+    keypress()
 
+    print("Stopping camera preview...")
+    camera_thread.join()
